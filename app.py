6ad9c2d2ea1ac8e0ed267481ede26088729889da
Preston Todd Cash <todd328@yahoo.com>
2025-05-05 10:47:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 # Serper API configuration
-SERPER_API_KEY = 'e367025275637ae71d660021a3199b0cb4c63cba'
+SERPER_API_KEY = 'YOUR-KEY'
 SERPER_API_URL = 'https://google.serper.dev/search'
 
 @app.route('/', methods=['GET', 'POST'])
